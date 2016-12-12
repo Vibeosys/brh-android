28de667af150cda92d05cff4923e75dc276083a0
ShrinivasSH <shrinivas@vibeosys.com>
2016-12-12 14:57:17 +0530

    Edit profile UI created
    Edit profile UI created

--- BusinessReviewsHub/app/src/main/java/com/businessreviewshub/fragments/BaseFragment.java
+++ BusinessReviewsHub/app/src/main/java/com/businessreviewshub/fragments/BaseFragment.java
@@ -1,7 +1,9 @@
 package com.businessreviewshub.fragments;
 
+import android.support.v4.app.Fragment;
+
 /**
  * Created by akshay on 12-12-2016.
  */
-public class BaseFragment {
+public class BaseFragment extends Fragment{
 }
---
+++ BusinessReviewsHub/app/src/main/java/com/businessreviewshub/views/RobotoBoldTextView.java
@@ -0,0 +1,25 @@
+package com.businessreviewshub.views;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by shrinivas on 12-12-2016.
+ */
+public class RobotoBoldTextView extends TextView {
+    public RobotoBoldTextView(Context context) {
+        super(context);
+    }
+
+    public RobotoBoldTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.setTypeface(Typeface.createFromAsset(context.getAssets(), "fonts/roboto.bold.ttf"));
+    }
+
+    public RobotoBoldTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.setTypeface(Typeface.createFromAsset(context.getAssets(), "fonts/roboto.bold.ttf"));
+    }
+}
