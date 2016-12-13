b5732c21b047331794ac4a34426fa468494f3905
AkshayVGadhave <akshay@vibeosys.com>
2016-12-13 16:11:35 +0530

    Changes in login screen
    Changes in login screen

--- BusinessReviewsHub/app/src/main/java/com/businessreviewshub/activities/LoginActivity.java
+++ BusinessReviewsHub/app/src/main/java/com/businessreviewshub/activities/LoginActivity.java
@@ -3,11 +3,16 @@ package com.businessreviewshub.activities;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.businessreviewshub.MainActivity;
 import com.businessreviewshub.R;
@@ -17,6 +22,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private Button mBtnLogin;
     private EditText mEdtCmpName;
     private Context mContext = this;
+    private TextView mTxtCopyright, mTxtWebsite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,18 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setTitle(getResources().getString(R.string.str_login_activity));
         mBtnLogin = (Button) findViewById(R.id.btn_login);
         mEdtCmpName = (EditText) findViewById(R.id.et_select_cmp);
+        mTxtCopyright = (TextView) findViewById(R.id.txtCopyRight);
+        mTxtWebsite = (TextView) findViewById(R.id.txtWebsite);
+        mTxtWebsite.setMovementMethod(LinkMovementMethod.getInstance());
+        SpannableStringBuilder ssWebsite = new SpannableStringBuilder(getString(R.string.str_login_website));
+        ssWebsite.setSpan(new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.vibeosys.com/"));
+                startActivity(browserIntent);
+            }
+        }, 43, 59, 0);
+        mTxtWebsite.setText(ssWebsite, TextView.BufferType.SPANNABLE);
         mBtnLogin.setOnClickListener(this);
         mEdtCmpName.setOnClickListener(this);
     }
