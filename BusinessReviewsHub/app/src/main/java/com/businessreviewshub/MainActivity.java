9b738977f0a012f5c2e882e594f22c98f1be4a22
ShrinivasSH <shrinivas@vibeosys.com>
2016-12-17 18:36:06 +0530

    bottom bar icons Updated
    bottom bar icons Updated

--- BusinessReviewsHub/app/src/main/java/com/businessreviewshub/MainActivity.java
+++ BusinessReviewsHub/app/src/main/java/com/businessreviewshub/MainActivity.java
@@ -4,11 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.businessreviewshub.fragments.EditProfileFragment;
 import com.businessreviewshub.fragments.HistoryDetailsFragment;
 import com.businessreviewshub.fragments.SendSMSFragment;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -16,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final String SMS_FRAGMENT = "sms";
     private static final String HISTORY_FRAGMENT = "history";
     private static final String PROFILE_FRAGMENT = "profile";
+    private TextView mSendSmsTv, mHistoryTv, mProfileTv;
+    private ImageView mSendSmsImg, mHistoryImg, mProfileImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mSendSmsLay = (LinearLayout) findViewById(R.id.sendSmsLay);
         mHistoryLay = (LinearLayout) findViewById(R.id.historyLay);
         mProfileLay = (LinearLayout) findViewById(R.id.profileLay);
+
+        mSendSmsTv = (TextView) findViewById(R.id.txtSendSms);
+        mHistoryTv = (TextView) findViewById(R.id.historyTxt);
+        mProfileTv = (TextView) findViewById(R.id.profileTxt);
+
+        mSendSmsImg = (ImageView) findViewById(R.id.sendSmsImg);
+        mHistoryImg = (ImageView) findViewById(R.id.historyImg);
+        mProfileImg = (ImageView) findViewById(R.id.profileImg);
+
 
         mSendSmsLay.setOnClickListener(this);
         mHistoryLay.setOnClickListener(this);
@@ -47,19 +62,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 SendSMSFragment sendSMSFragment = new SendSMSFragment();
                 getSupportFragmentManager().beginTransaction().
                         replace(R.id.fragment_frame_lay, sendSMSFragment, SMS_FRAGMENT).commit();
+                mSendSmsTv.setTextColor(getResources().getColor(R.color.colorBottom_yellow));
+                mHistoryTv.setTextColor(getResources().getColor(android.R.color.white));
+                mProfileTv.setTextColor(getResources().getColor(android.R.color.white));
+                mSendSmsImg.setImageDrawable(getResources().getDrawable(R.drawable.ic_send_sms_yellow_24));
+                mHistoryImg.setImageDrawable(getResources().getDrawable(R.drawable.ic_timer_white_24));
+                mProfileImg.setImageDrawable(getResources().getDrawable(R.drawable.ic_user_white_24));
                 break;
             case R.id.historyLay:
                 HistoryDetailsFragment historyDetailsFragment = new HistoryDetailsFragment();
                 getSupportFragmentManager().beginTransaction().
                         replace(R.id.fragment_frame_lay, historyDetailsFragment, HISTORY_FRAGMENT).commit();
+                mSendSmsTv.setTextColor(getResources().getColor(android.R.color.white));
+                mHistoryTv.setTextColor(getResources().getColor(R.color.colorBottom_yellow));
+                mProfileTv.setTextColor(getResources().getColor(android.R.color.white));
+                mSendSmsImg.setImageDrawable(getResources().getDrawable(R.drawable.ic_textsms_white_24dp));
+                mHistoryImg.setImageDrawable(getResources().getDrawable(R.drawable.ic_timer_yellow_24));
+                mProfileImg.setImageDrawable(getResources().getDrawable(R.drawable.ic_user_white_24));
                 break;
             case R.id.profileLay:
                 EditProfileFragment editProfileFragment = new EditProfileFragment();
                 getSupportFragmentManager().beginTransaction().
                         replace(R.id.fragment_frame_lay, editProfileFragment, PROFILE_FRAGMENT).commit();
+                mSendSmsTv.setTextColor(getResources().getColor(android.R.color.white));
+                mHistoryTv.setTextColor(getResources().getColor(android.R.color.white));
+                mProfileTv.setTextColor(getResources().getColor(R.color.colorBottom_yellow));
+                mSendSmsImg.setImageDrawable(getResources().getDrawable(R.drawable.ic_textsms_white_24dp));
+                mHistoryImg.setImageDrawable(getResources().getDrawable(R.drawable.ic_timer_white_24));
+                mProfileImg.setImageDrawable(getResources().getDrawable(R.drawable.ic_profile_yellow_24));
                 break;
             default:
-
+                mSendSmsTv.setTextColor(getResources().getColor(R.color.colorBottom_yellow));
+                mHistoryTv.setTextColor(getResources().getColor(android.R.color.white));
+                mProfileTv.setTextColor(getResources().getColor(android.R.color.white));
+                mSendSmsImg.setImageDrawable(getResources().getDrawable(R.drawable.ic_send_sms_yellow_24));
+                mHistoryImg.setImageDrawable(getResources().getDrawable(R.drawable.ic_timer_white_24));
+                mProfileImg.setImageDrawable(getResources().getDrawable(R.drawable.ic_user_white_24));
                 break;
         }
     }
